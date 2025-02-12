0884fb27250a2fc8fddf52997d15a7a825522232
bynd1u <semenbindu@gmail.com>
2025-02-12 16:33:43 +0100

    1-time-management completed

--- lesson-plans/devtools-debugger/2-integrate/exercises/2-conditionals/1-time-management/app.js
+++ lesson-plans/devtools-debugger/2-integrate/exercises/2-conditionals/1-time-management/app.js
@@ -20,14 +20,18 @@ whenFormDataChanges('sleep-info', () => {
   // --- generate advice ---
 
   let advice = '';
-  if (isTired && hasTime) {
-    advice = 'Take a nap!';
-  } else if (!isTired && hasTime) {
-    advice = 'Study some flashcards.';
-  } else if (isTired && !hasTime) {
-    advice = 'Make some coffee.';
-  } else if (!isTired && !hasTime) {
-    advice = 'Have a great day!';
+  if (isTired) {
+    if (hasTime) {
+      advice = 'Take a nap!';
+    } else {
+      advice = 'Make some coffee.';
+    }
+  } else {
+    if (hasTime) {
+      advice = 'Study some flashcards.';
+    } else {
+      advice = 'Have a great day!';
+    }
   }
 
   // --- display the advice ---
